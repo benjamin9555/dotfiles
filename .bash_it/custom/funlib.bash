f4da2438d7e5935f7e38092d1f97a217ba453dd2
Benjamin Gallusser <bgallusser@googlemail.com>
2020-09-18 12:04:49 -0400

    Add env vars to disable hidden numpy multithreading

--- .bash_it/custom/funlib.bash
+++ .bash_it/custom/funlib.bash
@@ -2,3 +2,8 @@
 
 # neuroglancer
 export PATH="$PATH:~/code/src/funlib.show.neuroglancer/scripts"
+
+# disable hidden openmp and therefore numpy multiprocessing
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
