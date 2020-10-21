969e4936005d946de8567be97a86767d369431e9
Benjamin Gallusser <bgallusser@googlemail.com>
2020-10-21 15:42:30 +0200

    Relax dgx tunneling regex

--- .bash_it/aliases/custom.aliases.bash
+++ .bash_it/aliases/custom.aliases.bash
@@ -8,7 +8,7 @@ dgx() {
     if [[ $port =~ ^[0-9]{5}$ ]]; then
         echo "Open tunnel to DGX port $port"
         ssh -NfL $port:127.0.0.1:$port bengallusser@dgx1
-    elif [[ $port =~ ^http:\/\/127\.0\.0\.1:([0-9]{5}).+$ ]]; then
+    elif [[ $port =~ ^http:\/\/.*:([0-9]{5}).+$ ]]; then
         # Regex group capturing from https://stackoverflow.com/questions/1891797/capturing-groups-from-a-grep-regex
         local port_regexed="${BASH_REMATCH[1]}"
         echo "Open tunnel to DGX port $port_regexed"
