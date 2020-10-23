1cab48c900fb7a28fbcbcb513917feaea4a2b451
Benjamin Gallusser <bgallusser@googlemail.com>
2020-10-23 19:15:20 +0200

    Add todo.txt custom alias, add alias comments

--- .bash_it/aliases/custom.aliases.bash
+++ .bash_it/aliases/custom.aliases.bash
@@ -1,4 +1,13 @@
+# Pretty ls
 alias ll='ls -lah'
+
+# Count all non-hidden files in a directory
 alias count='ls -1 | wc -l'
+
+# du sorted by name
 alias dusort='du -hs *'
+# du sorted by size
 alias dusize='du -hs * | sort -h'
+
+# List todo.txt task with prio A
+alias tla='t lsp A'
