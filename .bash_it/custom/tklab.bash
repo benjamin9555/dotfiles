34f654d52c32571684e78ccbb2fe7e5f27d5d92a
Benjamin Gallusser <bgallusser@googlemail.com>
2021-01-07 19:26:53 +0100

    Beautify neuroglancer tunneling script

--- .bash_it/custom/tklab.bash
+++ .bash_it/custom/tklab.bash
@@ -1,15 +1,17 @@
 dgx() {
     local port="$1"
+    local username="bengallusser"
+    local machine="dgx1"
     # get rid of the first argument
     shift 1
     if [[ $port =~ ^[0-9]{5}$ ]]; then
-        echo "Open tunnel to DGX port $port"
-        ssh -NfL $port:127.0.0.1:$port bengallusser@dgx1
+        echo "Open tunnel to $machine port $port"
+        ssh -NfL $port:127.0.0.1:$port ${username}@${machine}
     elif [[ $port =~ ^http:\/\/127\.0\.0\.1:([0-9]{5}).+$ ]]; then
         # Regex group capturing from https://stackoverflow.com/questions/1891797/capturing-groups-from-a-grep-regex
         local port_regexed="${BASH_REMATCH[1]}"
-        echo "Open tunnel to DGX port $port_regexed"
-        ssh -NfL $port_regexed:127.0.0.1:$port_regexed bengallusser@dgx1
+        echo "Open tunnel to $machine port $port_regexed"
+        ssh -NfL $port_regexed:127.0.0.1:$port_regexed ${username}@${machine}
     else
         echo "Did not find a valid tunnel in the input"
     fi
